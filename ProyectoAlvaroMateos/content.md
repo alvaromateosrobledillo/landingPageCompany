76c1ec783f6aac762c50c37acc76ff00330d5cc5
alvaro <alvaromar988@gmail.comalvaro>
2023-12-05 11:42:31 +0100

    landing a empresa real (servicio de entrenamiento)

---
+++ ProyectoAlvaroMateos/content.md
@@ -0,0 +1,113 @@
+## Acerca de VaritoFit
+
+Somos una empresa dedicada al bienestar personal a través del entrenamiento físico personalizado. Reconocemos que la vida moderna puede ser agitada, y dedicar tiempo al gimnasio puede resultar complicado. Es por eso que ofrecemos un enfoque innovador utilizando inteligencia artificial para adaptar los entrenamientos según tus preferencias y objetivos de acondicionamiento físico. No solo proporcionamos planes de ejercicio, sino que también te guiaremos en tu viaje hacia una vida más activa y saludable.
+
+## Marca
+
+Encabezado: Transforma tu cuerpo con VaritoFit
+
+Color de marca: #e67e22
+
+## Contenido del Sitio Web VaritoFit
+
+### Resumen
+
+La suscripción inteligente de fitness que te ayudará a entrenar efectivamente. Adaptado a tus gustos y necesidades de acondicionamiento físico. ¡Entregamos más de 500 entrenamientos personalizados cada año!
+
+### Características de VaritoFit
+
+¡Nunca dejes de entrenar!: Nuestras suscripciones cubren los 365 días del año, incluso en días festivos.
+Local y orgánico: Utilizamos métodos de entrenamiento basados en tu propio peso corporal.
+Cero residuos: Promovemos el uso de equipos reutilizables y sostenibles.
+Pausa en cualquier momento: ¿Planeas unas vacaciones? Simplemente pausa tu suscripción, ¡te reembolsamos los días no utilizados!
+
+### Cómo Funciona VaritoFit
+
+[Mostrar imágenes de la aplicación]
+
+**Tu dosis diaria de salud en 3 simples pasos**
+
+Indícanos tus preferencias: Nunca más pierdas tiempo pensando en qué entrenar. VaritoFit AI creará un plan de entrenamiento semanal personalizado para ti, asegurando que obtengas los nutrientes y la actividad que necesitas, según tus objetivos.
+
+Aprueba tu plan semanal: Una vez a la semana, aprueba el plan de entrenamiento personalizado creado por VaritoFit AI. Puedes ajustar ejercicios, cambiar rutinas o incluso agregar tus propias ideas.
+
+Recibe entrenamientos en casa: Nuestros entrenadores expertos diseñarán y guiarán tus entrenamientos diarios, y te los entregaremos en la comodidad de tu hogar. ¡Puedes cambiar tu horario y dirección de entrega todos los días!
+
+### VaritoFit funciona con cualquier objetivo
+
+- Pérdida de peso
+- Ganancia de fuerza
+- Resistencia cardiovascular
+- Flexibilidad y movilidad
+- Tonificación muscular
+
+### Ejemplos de Entrenamientos
+
+**VaritoFit AI elige entre 500+ rutinas**
+
+Entrenamiento 1: Rutina de Fuerza Corporal
+
+- Duración: 45 minutos
+- Objetivo: Ganancia de fuerza y tonificación
+- Nivel: Intermedio
+- Calificación promedio: 4.7
+
+Entrenamiento 2: Cardio Explosivo
+
+- Duración: 30 minutos
+- Objetivo: Resistencia cardiovascular y quema de calorías
+- Nivel: Principiante
+- Calificación promedio: 4.5
+
+### Ofrecemos una prueba de entrenamiento gratuita
+
+[Crea un formulario simple para que los usuarios se registren]
+
+Entrenamientos saludables, efectivos y sin complicaciones te esperan. Comienza tu viaje hacia una vida activa hoy. Puedes cancelar o pausar en cualquier momento. ¡Y el primer entrenamiento es gratis!
+
+### Nuestras Tarifas
+
+Los precios incluyen todos los impuestos aplicables. Los usuarios pueden cancelar en cualquier momento.
+
+Básico: $199 al mes
+
+- 3 entrenamientos por semana
+- Acceso a la comunidad en línea
+- Entrenamientos en vivo
+
+Completo: $349 al mes
+
+- Entrenamientos diarios
+- Acceso prioritario a nuevas rutinas
+- Chat en vivo con entrenadores expertos
+
+### Galería de Fotos
+
+[Usa las 12 fotos proporcionadas]
+
+### Testimonios de Clientes
+
+[Fotos de clientes incluidas]
+
+Entrenamientos efectivos y personalizados sin la necesidad de equipo especial. ¡Se siente verdaderamente increíble! (Ana Sánchez)
+La inteligencia artificial de VaritoFit ha transformado mi enfoque hacia el fitness. No puedo imaginar mi vida sin estos entrenamientos personalizados. (Carlos González)
+VaritoFit se ha convertido en mi compañero constante para mantenerme activo y saludable. ¡Altamente recomendado! (Laura Pérez)
+
+### Sección con logotipos de publicaciones destacadas [ver imágenes]
+
+### Información de Contacto
+
+Dirección: Calle del Dr. Fleming, 30, 28036 Madrid
+Teléfono: 666-666-666
+Correo Electrónico: info@varitofit.com
+
+Perfiles en Redes Sociales: Instagram, Facebook, Twitter [enlaces aún no disponibles]
+
+### Enlaces Adicionales [enlaces aún no disponibles]
+
+Crear cuenta
+Iniciar sesión
+Aplicación iOS
+Aplicación Android
+
+¡VaritoFit - Transformando vidas, un entrenamiento a la vez!
---
+++ ProyectoAlvaroMateos/js/script.js
@@ -0,0 +1,216 @@
+'use strict';
+
+const ventanaModal = document.querySelector('.modal');
+const superposicion = document.querySelector('.overlay');
+const btnCerrarVentanaModal = document.querySelector('.btn--close-modal');
+const btnsAbrirVentanaModal = document.querySelectorAll('.btn--show-modal');
+const btnDesplazamiento = document.querySelector('.btn--scroll-to');
+const seccion1 = document.querySelector('#section--1');
+const barraNavegacion = document.querySelector('.main-nav');
+const pestañas = document.querySelectorAll('.operations__tab');
+const contenedorPestañas = document.querySelector('.operations__tab-container');
+const contenidoPestañas = document.querySelectorAll('.operations__content');
+const seccionImagenes = document.querySelector('.section-testimonials');
+
+// Establecer el año actual
+const añoEl = document.querySelector(".year");
+const añoActual = new Date().getFullYear();
+añoEl.textContent = añoActual;
+
+// Hacer que la navegación móvil funcione
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const encabezadoEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  encabezadoEl.classList.toggle("nav-open");
+});
+
+// 1. Agregar event listener al elemento padre común
+// 2. Determinar qué elemento originó el evento
+
+document.querySelector('.main-nav-list').addEventListener('click', function (e) {
+  // Verificar si el enlace es interno o externo
+  const isInternalLink = e.target.classList.contains('main-nav-link') && e.target.getAttribute('href').startsWith('#');
+
+  // Si es un enlace interno, realiza el desplazamiento suave
+  if (isInternalLink) {
+    e.preventDefault();
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+
+    // Cerrar la navegación móvil
+    encabezadoEl.classList.toggle("nav-open");
+  }
+  // Si es un enlace externo, la navegación predeterminada ocurrirá
+});
+
+// Navegación  
+const seccionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+
+  function (entradas) {
+    const entrada = entradas[0];
+    console.log(entrada);
+
+    if (entrada.isIntersecting === false) {
+      document.body.classList.add("sticky");
+      console.log("el elemento es visible");
+    }
+
+    if (entrada.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+      console.log("el elemento no es visible");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(seccionHeroEl);
+
+// Antigua forma
+// const coordenadasIniciales = seccion1.getBoundingClientRect();
+// console.log(coordenadasIniciales);
+
+// window.addEventListener('scroll', function () {
+//   console.log(window.scrollY);
+
+//   if (window.scrollY > coordenadasIniciales.top) barraNavegacion.classList.add('sticky');
+//   else barraNavegacion.classList.remove('sticky');
+// });
+
+// Estilos de Cookie
+const estilosCookie = `
+  .cookie-message {
+    position: fixed;
+    bottom: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    width: 100%;
+    background-color: #99e9f2;
+    color: #bbb;
+    z-index: 303000;
+    padding: 10px
+  }
+
+  .cookie-message p {
+    margin: 0 0 1.6rem;
+    font-size: 1.6rem;
+    line-height: 1.6;
+      color: var(--grey-5);
+
+  }
+
+  .cookie-message button {
+  display: inline-block;
+
+  text-decoration: none;
+  font-size: 2rem;
+  font-weight: 600;
+  padding: 1.6rem 3.2rem;
+  border-radius: 9px;
+
+  /* Solo necesario para .btn */
+  border: none;
+  cursor: pointer;
+  font-family: inherit;
+
+  /* Poner transición en el "estado" original */
+  /* transition: background-color 0.3s; */
+  transition: all 0.3s;
+  }
+
+  .cookie-message button:hover {
+  background-color: var(--color-primario);
+  color: #fff;
+  }
+`;
+
+// Agregar estilos de Cookie
+const elementoEstilo = document.createElement('style');
+elementoEstilo.textContent = estilosCookie;
+document.head.append(elementoEstilo);
+
+// Mensaje de Cookie
+const piePagina = document.querySelector('.footer');
+const mensaje = document.createElement('div');
+const parrafo = document.createElement('p');
+mensaje.appendChild(parrafo);
+mensaje.classList.add('cookie-message');
+parrafo.innerHTML = '¡Bienvenido a [Nombre de tu Sitio]! Utilizamos cookies para ofrecerte una experiencia personalizada, mejorar la funcionalidad y realizar análisis de rendimiento. <button class="btn btn--outline btn--close">¡Entendido!</button>';
+barraNavegacion.append(mensaje);
+
+// Cerrar mensaje de Cookie
+document.querySelector('.btn--close').addEventListener('click', function () {
+  mensaje.parentElement.removeChild(mensaje);
+});
+
+// Animación de desvanecimiento del menú
+const manejarHover = function (e, opacidad) {
+  const enlace = e.target;
+
+  if (enlace.classList.contains('main-nav-link')) {
+    const hermanos = enlace.closest('.main-nav').querySelectorAll('.main-nav-link');
+
+    hermanos.forEach(el => {
+      if (el !== enlace) el.style.opacity = opacidad;
+    });
+  }
+};
+
+// Pasando "argumento" al controlador
+barraNavegacion.addEventListener('mouseover', function (e) {
+  manejarHover(e, 0.5)
+});
+barraNavegacion.addEventListener('mouseout', function (e) {
+  manejarHover(e, 1)
+});
+
+// Revelar secciones
+const todasLasSecciones = document.querySelectorAll('.section');
+console.log(todasLasSecciones);
+const revelarSeccion = function (entradas, observador) {
+  const [entrada] = entradas;
+
+  if (!entrada.isIntersecting) return;
+
+  entrada.target.classList.remove('section--hidden');
+  observador.unobserve(entrada.target);
+};
+
+const observadorSeccion = new IntersectionObserver(revelarSeccion, {
+  root: null,
+  threshold: 0.15,
+});
+
+todasLasSecciones.forEach(function (seccion) {
+  observadorSeccion.observe(seccion);
+  seccion.classList.add('section--hidden');
+});
+
+// Selecciona todas las imágenes de la galería
+const imagenesGaleria = document.querySelectorAll('.gallery-item img');
+
+// Función para manejar el evento hover
+const manejarHoverImagenes = function (opacidad) {
+  imagenesGaleria.forEach(img => {
+    img.style.opacity = opacidad;
+  });
+};
+
+// Agrega el manejador de eventos a cada imagen de la galería
+imagenesGaleria.forEach(img => {
+  img.addEventListener('mouseover', function () {
+    manejarHoverImagenes(1);
+  });
+
+  img.addEventListener('mouseout', function () {
+    manejarHoverImagenes(0.5);
+  });
+});
